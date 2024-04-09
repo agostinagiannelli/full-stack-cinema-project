4c4a59fda8aac514ec3a8ac5bf0b5360b2668fe4
Agostina Giannelli <agostinagiannelli@gmail.com>
2024-04-09 08:47:20 +0200

    Homework Clase 1

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,67 @@
-console.log(tempData);
+// class Movie {
+//     constructor(id, title, year, director, duration, genre, rate, poster) {
+//         this.id = id;
+//         this.title = title;
+//         this.year = year;
+//         this.director = director;
+//         this.duration = duration;
+//         this.genre = genre;
+//         this.rate = rate;
+//         this.poster = poster;
+//     };
+// };
+
+// class Repository {
+//     constructor() {
+//         this.movies = [];
+//         this.id = 0;
+//     };
+//     getAllMovies() {
+//         return this.movies;
+//     };
+//     addMovie(id, title, year, director, duration, genre, rate, poster) {
+//         this.id++;
+//         const movie = new Movie(this.id, title, year, director, duration, genre, rate, poster);
+//         this.movies.push(movie);
+//     };
+// };
+
+// const repository = new Repository();
+
+const featMovies = document.getElementById("featuredMoviesList");
+
+// Convert movie to HTML
+const movieToHtml = ({ id, title, year, director, duration, genre, rate, poster }) => {
+    const item = document.createElement("div");
+    item.innerHTML = `
+    <div class="card shadow-sm">
+        <img class="card-img-top object-fit-cover" height="350" src="${poster}" alt="${title}">
+        <div class="card-body">
+            <h5 class="card-title"><strong>${title}</strong></h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">${year}</h6>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Director: ${director}</li>
+            <li class="list-group-item">Duration: ${duration}</li>
+            <li class="list-group-item">Genres: ${genre[0]} · ${genre[1]} · ${genre[2]}</li>
+        </ul>
+        <div class="card-footer">
+            <p class="card-text">Rating: ⭐ ${rate}/10</p>
+        </div>
+    </div>
+    `;
+    item.classList.add("col");
+    return item;
+};
+
+// Render movies
+const renderMovie = () => {
+    // featMovies.innerHTML = "";
+    // const allMovies = repository.getAllMovies();
+    // const items = allMovies.map(item => movieToHtml(item));
+    // items.forEach(item => featMovies.append(item));
+    const items = tempData.map(item => movieToHtml(item));
+    items.forEach(item => featMovies.append(item));
+};
+
+renderMovie();
