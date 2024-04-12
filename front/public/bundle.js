76bc11e9a84264785e32b4e5aafefe7d56d96beb
Agostina Giannelli <agostinagiannelli@gmail.com>
2024-04-12 18:01:23 +0200

    Homework Clase 4 v2

--- front/public/bundle.js
+++ front/public/bundle.js
@@ -1,1 +1,66 @@
-(()=>{var t={945:t=>{const s=document.getElementById("featuredMoviesList");t.exports=t=>{t.map((t=>{const s=document.createElement("div");return s.classList.add("col"),s.innerHTML=`\n        <div class="card shadow-sm h-100">\n            <img class="card-img-top object-fit-cover" height="400" src="${t.poster}" alt="${t.title}">\n            <div class="card-body">\n                <h5 class="card-title"><strong>${t.title}</strong></h5>\n                <h6 class="card-subtitle mb-2 text-body-secondary">${t.year}</h6>\n            </div>\n            <ul class="list-group list-group-flush">\n                <li class="list-group-item">Director: ${t.director}</li>\n                <li class="list-group-item">Duration: ${t.duration}</li>\n                <li class="list-group-item">Genres: ${t.genre.join(" · ")}</li>\n            </ul>\n            <div class="card-footer">\n                <p class="card-text">⭐ ${t.rate}/10</p>\n            </div>\n        </div>\n        `,s})).forEach((t=>s.append(t)))}}},s={};function e(r){var i=s[r];if(void 0!==i)return i.exports;var n=s[r]={exports:{}};return t[r](n,n.exports,e),n.exports}(()=>{const t=e(945);$.get("https://students-api.up.railway.app/movies",((s,e)=>{t(s)}))})()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./scripts/index.js":
+/*!**************************!*\
+  !*** ./scripts/index.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("const renderMovies = __webpack_require__(/*! ./renderMovies */ \"./scripts/renderMovies.js\");\n\n$.get(\"https://students-api.up.railway.app/movies\", data => renderMovies(data)).fail(() => alert(\"Error when getting movies\"));\n\n//# sourceURL=webpack://front/./scripts/index.js?");
+
+/***/ }),
+
+/***/ "./scripts/renderMovies.js":
+/*!*********************************!*\
+  !*** ./scripts/renderMovies.js ***!
+  \*********************************/
+/***/ ((module) => {
+
+eval("const featMovies = document.getElementById(\"featuredMoviesList\");\n\nconst renderMovies = (data) => {\n    const items = data.map(movie => {\n        const item = document.createElement(\"div\");\n        item.classList.add(\"col\");\n        item.innerHTML = `\n        <div class=\"card shadow-sm h-100\">\n            <img class=\"card-img-top object-fit-cover\" height=\"400\" src=\"${movie.poster}\" alt=\"${movie.title}\">\n            <div class=\"card-body\">\n                <h5 class=\"card-title\"><strong>${movie.title}</strong></h5>\n                <h6 class=\"card-subtitle mb-2 text-body-secondary\">${movie.year}</h6>\n            </div>\n            <ul class=\"list-group list-group-flush\">\n                <li class=\"list-group-item\">Director: ${movie.director}</li>\n                <li class=\"list-group-item\">Duration: ${movie.duration}</li>\n                <li class=\"list-group-item\">Genres: ${movie.genre.join(' · ')}</li>\n            </ul>\n            <div class=\"card-footer\">\n                <p class=\"card-text\">⭐ ${movie.rate}/10</p>\n            </div>\n        </div>\n        `;\n        return item;\n    });\n    items.forEach(item => featMovies.append(item));\n};\n\nmodule.exports = renderMovies;\n\n//# sourceURL=webpack://front/./scripts/renderMovies.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./scripts/index.js");
+/******/ 	
+/******/ })()
+;
--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,5 +1,3 @@
 const renderMovies = require("./renderMovies");
 
-$.get("https://students-api.up.railway.app/movies", (data, status) => {
-    renderMovies(data);
-});
+$.get("https://students-api.up.railway.app/movies", data => renderMovies(data)).fail(() => alert("Error when getting movies"));
