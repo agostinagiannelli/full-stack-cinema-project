917205896e8de4f2c1c909a002b5121e5fd1e9dd
Agostina Giannelli <agostinagiannelli@gmail.com>
2024-04-15 20:03:35 +0200

    Homework Clase 6

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,3 +1,17 @@
 const renderMovies = require("./renderMovies");
 
-$.get("https://students-api.up.railway.app/movies", data => renderMovies(data)).fail(() => alert("Error when getting movies"));
+// $.get("https://students-api.up.railway.app/movies", data => renderMovies(data)).fail(() => alert("Error when getting movies"));
+
+const axios = require("axios");
+
+const getMovies = async () => {
+    try {
+        const response = await axios.get("https://students-api.up.railway.app/movies");
+        const data = response.data;
+        renderMovies(data);
+    } catch (error) {
+        alert("Error when getting movies");
+    }
+};
+
+getMovies();
